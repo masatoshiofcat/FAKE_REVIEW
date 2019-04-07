7b53ec37b1b929f3c7942ea3e690123a24aab9db
TSITCL\s172162 <hasegawamasatoshiofcat@gmail.com>
2019-04-08 07:48:00 +0900

    AssetタブよりCreateCardが選べるようになりました。

---
+++ HTTP_FAKERAVIEW_COM/Assets/Scripts/Hasegawa/CardBase.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardBase : MonoBehaviour
+{
+    [SerializeField]
+    Texture2D iconTexture;//商品のイメージアイコン
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //アイコンをイメージ位置に移動
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
