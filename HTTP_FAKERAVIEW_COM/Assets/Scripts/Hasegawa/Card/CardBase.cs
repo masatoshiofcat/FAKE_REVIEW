57475681ee41edd721f10c034f16f53960e769ab
TSITCL\s172162 <hasegawamasatoshiofcat@gmail.com>
2019-04-08 07:48:52 +0900

    CreateCardのバグを修正

---
+++ HTTP_FAKERAVIEW_COM/Assets/Scripts/Hasegawa/Card/CardBase.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 商品カードの基底クラス。
+/// 商品カードを作成するにはこれを継承したオブジェクトを生成する必要がある。
+/// </summary>
+abstract public class CardBase : MonoBehaviour
+{
+    [SerializeField]
+    private string productName;//商品名
+    [SerializeField]
+    private string[] tagNames;//タグ名s
+
+    [SerializeField]
+    private string mouseOverString;//アイコンに？マウスがのせられた際に表示するヒント
+    
+    [SerializeField]
+    private Texture2D iconTexture;//商品のイメージアイコン画像
+    [SerializeField]
+    private Vector3 iconOffst;//アイコンの描画位置
+
+    [SerializeField]
+    private int maxStock; //在庫
+
+
+    private int currentStock;//現在までに売れた個数
+    
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //アイコンをイメージ位置に移動
+    }
+
+    /// <summary>
+    /// カードの効果を発揮する純粋仮想関数
+    /// </summary>
+    public abstract void Effect();
+}
---
+++ HTTP_FAKERAVIEW_COM/Assets/Scripts/Hasegawa/Card/CardDataBase.cs
@@ -0,0 +1,20 @@
+﻿///カードのデータをまとめておくデータベースを作るためのクラス
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "CardDataBase",menuName ="CreateCardDataBase")]
+public class CardDataBase : ScriptableObject
+{
+    [SerializeField]
+    private List<CardData> cardDataList = new List<CardData>();
+
+    /// <summary>
+    /// カードのデータリストの取得
+    /// </summary>
+    /// <returns></returns>
+    public List<CardData> GetCardDataList()
+    {
+        return this.cardDataList;
+    }
+}
